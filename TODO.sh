97a760a0ec6d7a91679895a2a8b5efbff7310b77
Kenji De Coster <kenji.decoster@student.hogent.be>
2024-10-09 21:28:46 +0200

    Check in het vervolg u terminals beter gij kalf

--- TODO.sh
+++ TODO.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-pwd
-ls
-cd getting-started-master
-pwd
-ls
-cd app
-pwd
-ls
 
-#docker build -t getting-started .
-#docker run -dp 3000:3000 getting-started
+cd getting-started-master/app
+
+docker build -t getting-started .
+docker run -dp 3000:3000 getting-started
