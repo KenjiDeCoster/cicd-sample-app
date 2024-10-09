7cddc8c13ee64b1fe1f9dc97e8b7f04550cdba6a
Kenji De Coster <kenji.decoster@student.hogent.be>
2024-10-09 16:09:25 +0200

    uitbreiding 1

--- TODO.sh
+++ TODO.sh
@@ -1,43 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Variables
-REPO_B_URL="https://github.com/docker/getting-started/archive/refs/heads/main.zip"
-TARGET_DIR="C:\Users\kenji\Documents\- Hogent\Jaar 3\Infrastructure Automation\infra-24-25-KenjiDeCoster\dockerlab\cicd-sample-app"
-TEMP_DIR=$(mktemp -d)
-
-# Download the repository as a zip file
-curl -L $REPO_B_URL -o "$TEMP_DIR/repository_B.zip"
-
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download repository B."
-    exit 1
-fi
-
-# Unzip the downloaded file
-unzip "$TEMP_DIR/repository_B.zip" -d $TEMP_DIR
-
-# Check if the unzip was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to unzip the repository."
-    exit 1
-fi
-
-# Copy the specific folder from the unzipped content to repository A
-cp -r "$TEMP_DIR/repository_B-main/getting-started-master" "$TARGET_DIR"
-
-# Check if the copy was successful
-if [ $? -eq 0 ]; then
-    echo "Folder copied successfully!"
-else
-    echo "Failed to copy the folder."
-    exit 1
-fi
-
-# Clean up temporary files
-rm -rf $TEMP_DIR
-
 cd getting-started-master/app
 
 cat Dockerfile << _EOF_
